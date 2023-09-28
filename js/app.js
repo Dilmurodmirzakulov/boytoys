c087d640a959f146000f5bd50856b90f9e3b9df0
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-09-28 11:43:17 +0500

    updated

--- js/app.js
+++ js/app.js
@@ -239,3 +239,19 @@ $().ready(function () {
   });
   $("input[data-plugin='birthday-mask']").inputmask({ mask: "99.99.9999" });
 });
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true
+});
